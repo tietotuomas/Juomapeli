e91f6405d08710b995db93efe5db41b56b65718b
tietotuomas <tietotuomas@gmail.com>
2023-01-14 03:48:54 +0200

    Add some minor functionality, refactor old code. Add MUI UI library.

--- juomapeli/src/App.js
+++ juomapeli/src/App.js
@@ -1,25 +1,18 @@
 import { useState } from 'react'
+import { Typography, Button, Stack, TextField, Container } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send'
 
 const App = () => {
   const [name, setName] = useState('')
   const [players, setPlayers] = useState([])
   const [round, setRound] = useState(0)
+  const [maxRounds, setMaxRounds] = useState(0)
+  const [factor, setFactor] = useState(0)
   const [drinker, setDrinker] = useState(0)
-  const [cards, setCards] = useState([
-    'Juo ',
-    'Juo ',
-    'Juo ',
-    'Juo ',
-    'Tarjoa ',
-    'Tarjoa ',
-    'Tarjoa ',
-    'Tarjoa ',
-    'Vesiputous',
-    'Sanaleikki',
-    'Sääntö',
-    'Painostusbiisi',
-  ])
-  const [difficulty, setDifficulty] = useState(0)
+  const [rules, setRules] = useState([])
+  const [rule, setRule] = useState('Puhukaa englantia')
+  const [showRuleForm, setShowRuleForm] = useState(false)
+  const [cards, setCards] = useState([])
 
   const handleNewName = (event) => {
     event.preventDefault()
@@ -27,10 +20,24 @@ const App = () => {
     setName('')
   }
 
+  const handleNewRule = (event) => {
+    event.preventDefault()
+    setRules(rules.concat(rule))
+    setRule('')
+    setShowRuleForm(false)
+    nextRound()
+  }
+
   const quitGame = () => {
     setRound(0)
     setPlayers([])
     setDrinker(0)
+    setFactor(0)
+    setMaxRounds(0)
+    setRules([])
+    setRule('')
+    setCards([])
+    setShowRuleForm(false)
   }
 
   const nextRound = () => {
@@ -42,110 +49,213 @@ const App = () => {
     }
   }
 
-  const startGame= (level) => {
-    console.log("startGamea kutsuttiin");
+  const startGame = (level) => {
     setRound(1)
-    if (level==='kutsut') {
-      setDifficulty(5)
+    const cardsToBeAdded = []
+    if (level === 'kutsut') {
+      setFactor(5)
+      setMaxRounds(20)
+      for (let i = 0; i < 5; i++) {
+        cardsToBeAdded.push('Juo ')
+        cardsToBeAdded.push('Tarjoa ')
+      }
+      cardsToBeAdded.push(
+        ...[
+          'Painostusbiisi',
+          'Sääntö',
+          'Sääntö',
+          'Sanaleikki',
+          'Sanaleikki',
+          'Kaikki juo 1',
+          'Juokaa vettä/mehua/teetä',
+        ]
+      )
+      setCards(cardsToBeAdded)
     }
-    if (level==='tiistai') {
-      setDifficulty(15)
+    if (level === 'seitin') {
+      setFactor(10)
+      setMaxRounds(40)
+      for (let i = 0; i < 5; i++) {
+        cardsToBeAdded.push('Juo ')
+        cardsToBeAdded.push('Tarjoa ')
+      }
+      cardsToBeAdded.push(
+        ...[
+          'Painostusbiisi',
+          'Sääntö',
+          'Vesiputous',
+          'Sanaleikki',
+          'Kaikki juo 2',
+        ]
+      )
+      setCards(cardsToBeAdded)
     }
+    if (level === 'tiistai') {
+      setFactor(15)
+      setMaxRounds(52)
+      for (let i = 0; i < 4; i++) {
+        cardsToBeAdded.push('Juo ')
+        cardsToBeAdded.push('Tarjoa ')
+      }
+      cardsToBeAdded.push(
+        ...[
+          'Painostusbiisi',
+          'Sääntö',
+          'Vesiputous',
+          'Sanaleikki',
+          'Kaikki juo 2',
+          'Juo shotti',
+        ]
+      )
+      setCards(cardsToBeAdded)
+    }
+    // if (level === 'custom') {
+    //   VALITSE ITSE KORTIT?
+    // }
   }
 
   const drawButton = () => {
     if (round > 0) {
       return (
         <div>
-          <button type="submit" onClick={quitGame}>
+          <Button type="submit" onClick={quitGame}>
             Lopeta peli
-          </button>
+          </Button>
         </div>
       )
     }
     if (players.length > 1) {
-      console.log("drawButton-funkkarissa mentiin aloita-peli lohkoon");
       return (
         <div>
-          Aloita peli:
-          <br/>
-          <button type="submit" onClick={() => startGame('kutsut')}>
-            Teekutsut
-          </button>
-          <button type="submit" onClick={() => startGame('tiistai')}>
-            Tiistai-ilta
-          </button>
+          <Typography variant="h4">Aloita peli:</Typography>
+
+          <br />
+          <Stack spacing={1} direction="row">
+            <Button
+              variant="contained"
+              type="submit"
+              onClick={() => startGame('kutsut')}
+            >
+              Teekutsut
+            </Button>
+            <Button
+              variant="contained"
+              type="submit"
+              onClick={() => startGame('seitin')}
+            >
+              Seitin ohuet
+            </Button>
+            <Button
+              variant="contained"
+              type="submit"
+              onClick={() => startGame('tiistai')}
+            >
+              Tiistai-ilta
+            </Button>
+          </Stack>
         </div>
       )
     }
   }
 
   const drawCard = () => {
-    console.log('drawCrad-funkkarissa')
-    if (round > 3*difficulty) {
-
+    if (round > maxRounds) {
       quitGame()
     }
 
     if (round > 0) {
       return (
-        <p>
-          Vuorossa: {players[drinker]}
-          <br/>
-          <br/>
-          {pickCard()}
-          <br/>
-          <br/>
+        <div>
+          <br />
+          {shuffleCard()}
+          <br />
+          <br />
           <button onClick={nextRound}>Seuraava kierros</button>
-        </p>
+        </div>
       )
     }
-
   }
 
-  const pickCard = () => {
+  const shuffleCard = () => {
     const card = cards[Math.floor(Math.random() * cards.length)]
     if (card === 'Juo ') {
-      const amount = Math.floor(Math.random() * difficulty)+1
+      const amount = Math.floor(Math.random() * factor) + 1
       return <span>Juo {amount}</span>
     }
     if (card === 'Tarjoa ') {
-      const amount = Math.floor(Math.random() * difficulty)+1
+      const amount = Math.floor(Math.random() * factor) + 1
       return <span>Tarjoa {amount}</span>
+    }
+    if (card === 'Sääntö') {
+      setShowRuleForm(true)
     }
     return <span>{card}</span>
   }
 
-  const drawAddPlayer = () => {
+  const addPlayer = () => {
     if (round < 1) {
       return (
         <form onSubmit={handleNewName}>
-          <input
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            type="text"
-          ></input>
-          <button type="submit">Lisää pelaaja</button>
+          <Stack spacing={1} direction="row">
+            <TextField
+              size="small"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              type="text"
+            ></TextField>
+            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+              Lisää pelaaja
+            </Button>
+          </Stack>
         </form>
       )
     }
   }
 
+  const addRule = () => {
+    return (
+      <form onSubmit={handleNewRule}>
+        <input
+          type="text"
+          value={rule}
+          onChange={(event) => setRule(event.target.value)}
+        ></input>
+        <button type="submit">Lisää sääntö</button>
+      </form>
+    )
+  }
+
   return (
-    <div>
-      <h1>Juomapeli</h1>
-      {drawAddPlayer()}
+    <Container>
+      <Typography variant="h1" gutterBottom>
+        Juomapeli
+      </Typography>
+
+      {addPlayer()}
       <ul>
-        <p>
-          {players.map((p) => (
-            <li key={p}>{p}</li>
-          ))}
-        </p>
+        {players.map((p) => (
+          <li key={p}>{p}</li>
+        ))}
       </ul>
 
       {drawButton()}
-      {drawCard()}
-    </div>
+      {round > 0 ? (
+        <div>
+          {`Vuorossa: ${players[drinker]}`}
+          <br />
+          {`Kierros: ${round}/${maxRounds}`}
+        </div>
+      ) : null}
+
+      {showRuleForm === false && drawCard()}
+      {showRuleForm === true && addRule()}
+      {rules.length > 0 ? <Typography variant="h3">Säännöt:</Typography> : null}
+      <ul>
+        {rules.map((r) => (
+          <li key={r}>{r}</li>
+        ))}
+      </ul>
+    </Container>
   )
 }
 
